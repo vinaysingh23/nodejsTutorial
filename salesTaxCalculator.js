fa61a4e73b47063f699a89f18b709aa176c4fb3f
vinaysingh23 <vksingh@grepruby.com>
2017-07-07 18:42:03 +0530

    updatedWithArrayObject

--- salesTaxCalculator.js
+++ salesTaxCalculator.js
@@ -7,92 +7,89 @@
 /*let shopingInput = ["1 imported box of chocolates at 10.00",
 "1 imported bottle of perfume at 47.50"];
 */
-let shopingInput = ['1 book at 12.49',
+/*let shopingInput = ['1 book at 12.49',
 '1 music CD at 14.99',
-'1 chocolates bar at 0.85'];
+'1 chocolates bar at 0.85'];*/
 
-let iterator = 0;
+let shopingInput = [
+	{noOfItem: 1 , type: 'book' , imported: 'no' ,price: 12.49  },
+	{noOfItem: 1 , type: 'music CD' , imported: 'no' ,price: 14.99  },
+	{noOfItem: 1 , type: 'chocolates bar' , imported: 'no' ,price: 0.85  }
+];
+
+
 let totalPrice = 0;
-let salsetaxes = 0 ;
-while(iterator < shopingInput.length){
+let salesTaxes = 0 ;
+shopingInput.forEach(function(item) {
 
-	let resultInput = shopingInput[iterator].split(' ');
-	let outputString='';
-	for(let i=0;i<resultInput.length-2;i++)
-	{
-	     outputString = outputString + ' ' +resultInput[i];
-	}
-	
-	
-
-	let itemPrice = (Number(resultInput[resultInput.length-1]));
-	
-	
-
-	let Imported = shopingInput[iterator].indexOf("imported");
-
-	let Currenttaxes=0;
-	if(Imported>=0){
+//console.log(item.price);
+	let CurrentTaxes=0;
+	if(item.imported == 'yes'){
 
 
-		Currenttaxes += (taxForImpoted(itemPrice));
+		CurrentTaxes += (taxForImpoted(item.price));
 		
 	}
 	
 	
-	if(((shopingInput[iterator].indexOf("book")>=0) || (shopingInput[iterator].indexOf("chocolates")>=0) || (shopingInput[iterator].indexOf("medical") >=0) || (shopingInput[iterator].indexOf("pills")>=0))){
-	    
-	    Currenttaxes += 0;
+	if(item.type =='chocolates bar' || item.type =='pills' || item.type =='book'  ){
+
+
 	}else{
 		
 
-		Currenttaxes+= (taxForNonSpacialGoods(itemPrice));
+		CurrentTaxes+= (taxForNonSpacialGoods(item.price));
 
 	}
 
-    totalPrice =ToAddFloat(totalPrice,itemPrice) ;
+    totalPrice =toAddFloat(totalPrice,item.price) ;
     
 
-	itemPrice =ToAddFloat(itemPrice, Number( (Math.ceil(Currenttaxes*20)/20).toFixed(2)));
+	itemPrice =toAddFloat(item.price, Number( (Math.ceil(CurrentTaxes*20)/20).toFixed(2)));
 	
 
-	salsetaxes =ToAddFloat(salsetaxes, Number( (Math.ceil(Currenttaxes*20)/20).toFixed(2)));
+	salesTaxes =toAddFloat(salesTaxes, Number( (Math.ceil(CurrentTaxes*20)/20).toFixed(2)));
 	
+    if(item.imported=='yes'){
 
-	console.log(`${outputString} : ${itemPrice}` );
-	iterator++;
-}
+		console.log(`${item.noOfItem} imported ${item.type}: ${itemPrice}` );
+	}else{
+
+		console.log(`${item.noOfItem} ${item.type}: ${itemPrice}` );
+	}
+	
+});
 
 function taxForImpoted(itemPrice)
 {
-	let Currenttaxes = 0;
-		Currenttaxes =ToAddFloat(0, ((5*itemPrice)/100) );
-		Currenttaxes = Number( (Math.ceil(Currenttaxes*20)/20).toFixed(2));
+	let CurrentTaxes = 0;
+		CurrentTaxes =toAddFloat(0, ((5*itemPrice)/100) );
+		CurrentTaxes = Number( (Math.ceil(CurrentTaxes*20)/20).toFixed(2));
 		
-    return Currenttaxes;
+    return CurrentTaxes;
 		
 }
 
 
 function taxForNonSpacialGoods(itemPrice)
 {
-	let Currenttaxes = 0;
-	    Currenttaxes =ToAddFloat(0, (10*itemPrice)/100);
-	    Currenttaxes =  Number((Math.ceil(Currenttaxes*20)/20).toFixed(2));
+	let CurrentTaxes = 0;
+	    CurrentTaxes =toAddFloat(0, (10*itemPrice)/100);
+	    CurrentTaxes =  Number((Math.ceil(CurrentTaxes*20)/20).toFixed(2));
 		
-		return Currenttaxes;
+		return CurrentTaxes;
 }
-function ToAddFloat(number1,number2)
+function toAddFloat(number1,number2)
 {
 
    let totalPrice =parseFloat(number1) + parseFloat(number2) ;
     totalPrice = Number(totalPrice.toFixed(2));
     return totalPrice;
 }
-salsetaxes = (Math.ceil(salsetaxes*20)/20).toFixed(2);
+salesTaxes = (Math.ceil(salesTaxes*20)/20).toFixed(2);
 
 //totalPrice = (Math.ceil(totalPrice*20)/20).toFixed(2);
 console.log(totalPrice);
-totalPrice = Number(totalPrice)+Number(salsetaxes);
-console.log(`sales taxes : ${salsetaxes}`);
+totalPrice = Number(totalPrice)+Number(salesTaxes);
+console.log(`sales taxes : ${salesTaxes}`);
 console.log(`total : ${totalPrice}`);
